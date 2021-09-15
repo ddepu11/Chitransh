6cbbfd0c6f71a16275c46a653c921921949938ad
Deepanshu Tiwari <ddepu11@gmail.com>
2021-09-15 23:18:29 +0530

    chore: add CssMinimizerPlugin

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,6 +1,12 @@
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
 module.exports = {
   mode: "production",
   plugins: [new CleanWebpackPlugin()],
+
+  optimization: {
+    minimizer: [new CssMinimizerPlugin()],
+  },
 };
