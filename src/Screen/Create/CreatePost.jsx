9e47d58639dd86ef79bbe4c23a910b3e05a90a04
Deepanshu Tiwari <ddepu11@gmail.com>
2021-10-01 20:21:17 +0530

    fix: fix create post dialog

--- src/Screen/Create/CreatePost.jsx
+++ src/Screen/Create/CreatePost.jsx
@@ -164,8 +164,9 @@ const CreatePost = ({ handleCloseCreatePost }) => {
 };
 
 const Wrapper = styled.main`
-  position: absolute;
+  position: fixed;
   top: 0;
+  bottom: 0;
   width: 100%;
   height: 100%;
   background: rgba(0, 0, 0, 0.65);
