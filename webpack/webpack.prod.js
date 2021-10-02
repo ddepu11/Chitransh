e73560e22602bb23f6ec45f7a8157da5e5181e06
Deepanshu Tiwari <ddepu11@gmail.com>
2021-10-02 09:29:40 +0530

    fix: error on refresh on production

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,11 +1,21 @@
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-
-const TerserPlugin = require('terser-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const path = require('path');
 
 module.exports = {
   mode: 'production',
 
-  plugins: [new CleanWebpackPlugin()],
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, '../src/_redirects'),
+          to: path.resolve(__dirname, '../build'),
+        },
+      ],
+    }),
+  ],
 
   devtool: 'source-map',
 
