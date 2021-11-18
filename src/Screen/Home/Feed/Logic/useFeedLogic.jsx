f779cbe51cfe5827d31974095ed3864242f30129
Deepanshu Tiwari <ddepu11@gmail.com>
2021-11-19 00:09:40 +0530

    refactor: remove follow functionality

--- src/Screen/Home/Feed/Logic/useFeedLogic.jsx
+++ src/Screen/Home/Feed/Logic/useFeedLogic.jsx
@@ -1,21 +1,7 @@
-import {
-  collection,
-  where,
-  onSnapshot,
-  query,
-  doc,
-  updateDoc,
-  arrayUnion,
-} from 'firebase/firestore';
+import { collection, where, onSnapshot, query } from 'firebase/firestore';
 import { useEffect, useRef, useState } from 'react';
 import { useDispatch } from 'react-redux';
-import useNotificationOperations from '../../../../Components/useNotificationOperations';
-import useUserOperation from '../../../../Components/useUserOperations';
 import { firestoreInstance } from '../../../../config/firebase';
-import {
-  notificationShowError,
-  notificationShowInfo,
-} from '../../../../features/notification';
 
 const useFeedLogic = (info, id) => {
   const dispatch = useDispatch();
@@ -60,60 +46,7 @@ const useFeedLogic = (info, id) => {
     };
   }, [dispatch, id, info.following]);
 
-  const { getUpdatedUserDoc } = useUserOperation();
-  const { sendNotification } = useNotificationOperations();
-
-  const [followLoading, setFollowLoading] = useState(false);
-
-  const followAPerson = async (e) => {
-    setFollowLoading(true);
-
-    const personDocId = e.target.getAttribute('data-value');
-
-    try {
-      // Adding my id in person's followers array whom you  gonna follow
-      await updateDoc(doc(firestoreInstance, 'users', personDocId), {
-        followers: arrayUnion(id),
-      });
-
-      // Add person's id whom you gonna follow, in logged in user's following array
-      const userRef = doc(firestoreInstance, 'users', id);
-
-      await updateDoc(userRef, {
-        following: arrayUnion(personDocId),
-      });
-
-      // Send notification
-      const notification = {
-        body: `has started following you`,
-        sendToUserId: personDocId,
-        whoMade: {
-          userName: info.userName,
-          userId: id,
-          userDpUrl: info.dp.url,
-        },
-        postId: null,
-        postImg: '',
-        createdOn: Date.now(),
-      };
-
-      await sendNotification(personDocId, notification);
-
-      await getUpdatedUserDoc(id);
-
-      setFollowLoading(false);
-
-      dispatch(
-        notificationShowInfo({ msg: 'Successfully followed a person!' })
-      );
-    } catch (err) {
-      dispatch(notificationShowError({ msg: err.code.toString().slice(5) }));
-
-      setFollowLoading(true);
-    }
-  };
-
-  return { users, loading, followAPerson, followLoading };
+  return { users, loading };
 };
 
 export default useFeedLogic;
